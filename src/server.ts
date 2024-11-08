1d891fa3f83cd8bb5e8d3b91f166450086afe917
shiva <shiva@shiva.com>
2024-11-08 16:47:43 +0530

    get booking using ID

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,18 @@ app.get('/api/bookings', (req, res) => {
     res.json(readBookings());
 });
 
+app.get('/api/bookings/:id', (req: Request, res: Response) => {
+    const bookingId = parseInt(req.params.id, 10);
+    const bookings = readBookings();
+    const booking = bookings.find((b) => b.id === bookingId);
+
+    if (booking) {
+        res.json(booking);
+    } else {
+        res.status(404).json({ message: 'Booking not found' });
+    }
+});
+
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`);
 });
