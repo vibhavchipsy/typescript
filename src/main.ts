60eb66bffb4b46404ba5b9b222cfd0adf5b1a121
shiva <shiva@shiva.com>
2024-11-08 11:52:40 +0530

    Add a typed function for addition

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,9 @@ const year: number = 2024;
 const isTypescriptFun: boolean = true;
 
 console.log(greeting, year, isTypescriptFun);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log("Sum of 3 and 7 is " + add(3, 7));
