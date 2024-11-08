7e660842ef3b220874b62b0ded0495acc131cff0
shiva <shiva@shiva.com>
2024-11-08 11:44:01 +0530

    Add basic TypeScript types example

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-console.log("Hello, TypeScript!");
+const greeting: string = "Hello";
+const year: number = 2024;
+const isTypescriptFun: boolean = true;
+
+console.log(greeting, year, isTypescriptFun);
