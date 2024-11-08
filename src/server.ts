3a055752f875e32db88431e9658a63a8a7dd81c6
shiva <shiva@shiva.com>
2024-11-08 16:26:14 +0530

    get all bookings

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+app.get('/api/bookings', (req, res) => {
+    res.json(readBookings());
+});
+
+app.listen(port, () => {
+    console.log(`server is running on http://localhost:${port}`);
+});
