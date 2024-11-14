8fd7a36d3bee368cd0342cd4f5959c9ee255e085
shiva <shiva@shiva.com>
2024-11-14 11:13:51 +0530

    bookingControllers

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -1,1 +1,24 @@
 // Handles business logic
+import { Request, Response } from "express";
+import { Booking } from "../models/bookingModel";
+
+export const getBookings = async (req: Request, res: Response) => {
+    const bookings = await Booking.find();
+    res.json(bookings);
+};
+
+export const createBooking = async (req: Request, res: Response) => {
+    const newBooking = new Booking(req.body);
+    await newBooking.save();
+    res.status(201).json(newBooking);
+};
+
+export const updateBooking = async (req: Request, res: Response) => {
+    const updatedBooking = await Booking.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedBooking);
+};
+
+export const deleteBooking = async (req: Request, res: Response) => {
+    await Booking.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+};
