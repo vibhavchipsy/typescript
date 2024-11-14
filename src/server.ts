76f7b10c9e1f30c8c4b76aa2e0f3cc499c0a9057
shiva <shiva@shiva.com>
2024-11-14 17:23:16 +0530

    removed file based database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import { readBookings, createBooking, updateBooking, deleteBooking } from './main';
 import connectDB from './config/database';
 import bookingRoutes from './routes/bookingRoutes';
 
@@ -13,39 +12,6 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use('/api/bookings', bookingRoutes);
-
-// Route to get a booking by ID
-app.get('/api/bookings/:id', (req: Request, res: Response) => {
-    const bookingId = parseInt(req.params.id, 10);
-    const bookings = readBookings();
-    const booking = bookings.find((b) => b.id === bookingId);
-
-    if (booking) {
-        res.json(booking);
-    } else {
-        res.status(404).json({ message: 'Booking not found' });
-    }
-});
-
-// Route to create a new booking
-app.post('/api/bookings', (req: Request, res: Response) => {
-    const newBooking = req.body;
-    createBooking(newBooking);
-    res.status(201).json(newBooking);
-});
-
-app.put('/api/bookings/:id', (req, res) => {
-    const bookingId = parseInt(req.params.id, 10);
-    const updatedInfo = req.body;
-    updateBooking(bookingId, updatedInfo);
-    res.status(200).json({ message: "booking updated successfully" });
-});
-
-app.delete('/api/bookings/:id', (req: Request, res: Response) => {
-    const bookingId = parseInt(req.params.id, 10);
-    deleteBooking(bookingId);
-    res.status(200).json({ message: 'Booking deleted successfully' });
-});
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
