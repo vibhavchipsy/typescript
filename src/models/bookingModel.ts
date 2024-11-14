809e11a2664f25157ce004d2210124ac0ae3a805
shiva <shiva@shiva.com>
2024-11-14 11:05:59 +0530

    bookingModel

--- src/models/bookingModel.ts
+++ src/models/bookingModel.ts
@@ -1,1 +1,9 @@
 // Database schemas and models
+import mongoose from "mongoose";
+
+const BookingSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    date: { type: Date, required: true },
+});
+
+export const Booking = mongoose.model('Booking', BookingSchema);
