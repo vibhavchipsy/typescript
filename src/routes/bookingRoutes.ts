b8ba9af41464ba324aa78a9eff7744c86d98f10c
shiva <shiva@shiva.com>
2024-11-14 11:19:37 +0530

    bookingRoutes

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,1 +1,12 @@
 // Define API endpoints
+import express from "express";
+import { getBookings, createBooking, updateBooking, deleteBooking } from "../controllers/bookingController";
+
+const router = express.Router();
+
+router.get('/', getBookings);
+router.post('/', createBooking);
+router.put('/:id', updateBooking);
+router.delete(':id', deleteBooking);
+
+export default router;
